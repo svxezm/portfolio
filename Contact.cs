79cf506dd4bbbf41b3eacecdd3ec1f46f94f67ea
monitz <igorb.kuhl@gmail.com>
2024-06-14 18:13:50 -0300

    Created class as template for ContactLink component.

---
+++ Contact.cs
@@ -0,0 +1,9 @@
+namespace Portfolio;
+
+internal class Contact
+{
+    internal string[] IconPaths { get; set; } = Array.Empty<string>();
+    internal string[] Titles { get; set; } = Array.Empty<string>();
+    internal string[] Urls { get; set; } = Array.Empty<string>();
+    internal string[] Texts { get; set; } = Array.Empty<string>();
+}
