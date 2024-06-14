0aa64b5b2cc27a4a6b2a166a18161c3da7cd1494
monitz <igorb.kuhl@gmail.com>
2024-06-14 15:06:39 -0300

    Created classes as templates to render a list of components.

---
+++ LanguageBlockInfo.cs
@@ -0,0 +1,8 @@
+namespace Portfolio;
+
+internal class LanguageBlockInfo
+{
+    internal string[] Labels { get; set; } = Array.Empty<string>();
+    internal string[][] IconPaths { get; set; } = new string[2][];
+    internal string[][] Titles { get; set; } = new string[2][];
+}
