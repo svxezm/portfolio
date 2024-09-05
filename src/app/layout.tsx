a18f9ca9d2b0188c04243fa5136656bfc50cdf94
svxezm <igorb.kuhl@gmail.com>
2024-09-05 12:22:47 -0300

    Set page language to pt-BR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={montserrat.className}>{children}</body>
     </html>
   );
