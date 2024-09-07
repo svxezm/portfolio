d2f79ff8810dcd5c052f8ecec7b78d016acc1dcf
svxezm <igorb.kuhl@gmail.com>
2024-09-07 12:21:51 -0300

    feat: add a navigation header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,28 @@ import Image from "next/image";
 import MainLayout from "@/components/MainLayout";
 
 export default function Home() {
-  return (
-    <main
-    className="h-full flex flex-col items-center justify-between p-0 lg:p-8 lg:p-24"
-        style={{
-            backgroundImage: `url(/starry-background.png)`,
-            backgroundRepeat: "repeat-y",
-            backgroundAttachment: "scroll",
-            backgroundPosition: "top",
-            height: "100%"
-        }}
-    >
-        <MainLayout />
-    </main>
-  );
+    return (
+        <>
+            <header className="h-24 w-full bg-rose-200 drop-shadow-lg flex items-center justify-end">
+                <nav className="mr-8">
+                    <div>
+                        <a href="#" className="mr-2">Português</a>
+                        <a href="#">English</a>
+                    </div>
+                </nav>
+            </header>
+            <main
+                className="h-full flex flex-col items-center justify-between p-0 lg:p-8 lg:p-24"
+                style={{
+                    backgroundImage: `url(/starry-background.png)`,
+                    backgroundRepeat: "repeat-y",
+                    backgroundAttachment: "scroll",
+                    backgroundPosition: "top",
+                    height: "100%"
+                }}
+            >
+                <MainLayout />
+            </main>
+        </>
+    );
 }
