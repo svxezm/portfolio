811bf5e04e506500856fc8bee22d9b017de93232
svxezm <igorb.kuhl@gmail.com>
2024-09-08 07:28:23 -0300

    feat: add a dropdown menu to list available languages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
+"use client"
+
+import { useState } from "react";
 import Image from "next/image";
 import MainLayout from "@/components/MainLayout";
 
 export default function Home() {
+    const [isLangMenuOpen, setIsLangMenuOpen] = useState(false);
+
+    const toggleLangMenu = () => setIsLangMenuOpen(!isLangMenuOpen);
+
     return (
         <>
             <header className="h-24 w-full bg-rose-200 drop-shadow-lg flex items-center justify-end">
-                <nav className="mr-8">
-                    <div>
+                <nav className="mr-8 relative">
+                    <button onClick={toggleLangMenu}>Idiomas</button>
+                    <div
+                    className={`
+                        absolute end-0 flex flex-col 
+                        overflow-hidden transition-all duration-300 bg-rose-300 
+                        ${isLangMenuOpen ? "max-h-72 p-4" : "max-h-0 p-0"}
+                    `}>
                         <a href="#" className="mr-2">Português</a>
                         <a href="#">English</a>
                     </div>
