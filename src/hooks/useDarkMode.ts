065d9b5bcbfedd1623c2c57917ce0cd067fa7d4d
svxezm <igorb.kuhl@gmail.com>
2024-09-15 20:09:37 -0300

    Move dark mode logic to its own hook

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import $ from "jquery";
+
+export default function useDarkMode(initialState) {
+    const [darkMode, setDarkMode] = useState(true);
+    const html = document.querySelector("html");
+
+    useEffect(() => {
+        setDarkMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
+        if (darkMode && html.classList.length === 0) {
+            html.classList.add("dark");
+        }
+    }, []);
+
+    const switchTheme = () => {
+        setDarkMode(!darkMode)
+        html.classList.toggle("dark");
+    }
+
+    return {
+      darkMode,
+      setDarkMode,
+      switchTheme
+    };
+}
