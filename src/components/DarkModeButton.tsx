239f8383b227bae2cc477eccc94b679978832257
svxezm <igorb.kuhl@gmail.com>
2024-09-15 20:38:36 -0300

    Remove unecessary function

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -5,13 +5,9 @@ import { MoonStar, Sun } from "lucide-react";
 export default function DarkModeButton() {
     const { darkMode, switchTheme } = useDarkMode();
 
-    const changeTheme = () => {
-        switchTheme();
-    }
-
     return (
         <button
-            onClick={changeTheme}
+            onClick={switchTheme}
             className="rounded-full text-wine/50 dark:text-bubblegum/50 hover:text-wine dark:hover:text-bubblegum p-2 transition">
         {darkMode === false ? (
             <Sun />
