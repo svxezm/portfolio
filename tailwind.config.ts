30f4372a1ab08eac6e69dc0ac0cc98a0202ba2a4
svxezm <igorb.kuhl@gmail.com>
2024-09-15 15:17:42 -0300

    Add licorice and bubblegum colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "selector",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +16,8 @@ const config: Config = {
       },
       colors: {
           "wine": "hsl(var(--wine))",
+          "licorice": "#1a0008",
+          "bubblegum": "#cea1b7",
       },
     },
   },
