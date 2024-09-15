8862d5b526318bef5e5c67015eacc301b92a636c
svxezm <igorb.kuhl@gmail.com>
2024-09-15 20:39:16 -0300

    fix: avoid using document on top-level area to prevent errors about not being server-side defined

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -2,23 +2,24 @@ import { useState, useEffect } from "react";
 
 export default function useDarkMode(initialState) {
     const [darkMode, setDarkMode] = useState(true);
-    const html = document.querySelector("html");
 
     useEffect(() => {
+        const html = document.querySelector("html");
         setDarkMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
+
         if (darkMode && html.classList.length === 0) {
             html.classList.add("dark");
         }
-    }, []);
+    }, [darkMode]);
 
     const switchTheme = () => {
+        const html = document.querySelector("html");
         setDarkMode(!darkMode)
         html.classList.toggle("dark");
     }
 
     return {
       darkMode,
-      setDarkMode,
       switchTheme
     };
 }
