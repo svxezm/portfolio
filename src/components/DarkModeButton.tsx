3a5eb6bd0c3c84b28edb273e185fd74bc9152a12
svxezm <igorb.kuhl@gmail.com>
2024-09-15 20:18:04 -0300

    Use dark mode hook for a cleaner code

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
+import useDarkMode from "@/hooks/useDarkMode";
 import { MoonStar, Sun } from "lucide-react";
-import $ from "jquery";
-
 
 export default function DarkModeButton() {
-    const [darkMode, setDarkMode] = useState(false);
+    const { darkMode, switchTheme } = useDarkMode();
 
     const changeTheme = () => {
-        setDarkMode(!darkMode);
-        $("html").toggleClass("dark");
+        switchTheme();
     }
 
     return (
         <button
             onClick={changeTheme}
-            className="rounded-full text-wine/50 dark:text-bubblegum/50 hover:text-wine p-2 transition">
+            className="rounded-full text-wine/50 dark:text-bubblegum/50 hover:text-wine dark:hover:text-bubblegum p-2 transition">
         {darkMode === false ? (
             <Sun />
         ) : (
