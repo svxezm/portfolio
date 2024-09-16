f90f3ceacf30f0e1ec226ae2a4224c438ca94a5a
svxezm <igorb.kuhl@gmail.com>
2024-09-16 11:16:58 -0300

    Apply color variables for a cleaner code

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -5,7 +5,7 @@ import ProjectDisplay from "@/components/ProjectDisplay";
 export default function MainLayout() {
     return (
         <div className={
-            `lg:rounded-2xl bg-[#ffe5ed] dark:bg-[#300e24] w-screen lg:w-[70rem] p-8`
+            `lg:rounded-2xl bg-pink-light dark:bg-purple-dark w-screen lg:w-[70rem] p-8`
         }>
             <div className="w-[80%] mx-auto">
                 <TopContent />
