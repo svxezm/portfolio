c34cf38a7b8bbe972c6f5777396148fdb3b1c66a
svxezm <igorb.kuhl@gmail.com>
2024-09-16 11:29:39 -0300

    Move the header content to Header component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,8 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useTranslations } from "next-intl";
-import TranslationMenu from "@/components/TranslationMenu";
-import DarkModeButton from "@/components/DarkModeButton";
+import Header from "@/components/Header";
 import MainLayout from "@/components/MainLayout";
 import starryBackground from "/public/starry-background.png";
 
@@ -35,25 +34,7 @@ export default function Home() {
 
     return (
         <>
-        <header className="h-24 w-full bg-rose-200 dark:bg-licorice drop-shadow-lg flex items-center text-xl">
-                <nav className="w-[70rem] px-4 lg:px-0 mx-auto flex justify-between items-center">
-                    <div className="flex items-baseline">
-                        <TranslationMenu />
-                        <DarkModeButton />
-                    </div>
-                    <div>
-                        {nav.links.map((link, index) => (
-                            <a
-                                key={index}
-                                href={nav.ids[index]}
-                                className={`
-                                    mr-8 hover:text-rose-900 dark:hover:text-rose-200 transition-all duration-300 
-                                `}
-                            >{link}</a>
-                        ))}
-                    </div>
-                </nav>
-            </header>
+            <Header nav={nav} />
             <main
                 className="h-full flex flex-col items-center justify-between p-0 lg:p-8 lg:p-24"
                 style={
