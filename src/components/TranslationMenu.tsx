9e28ae61e5de53f204b5384509d2edd05b3a8986
svxezm <igorb.kuhl@gmail.com>
2024-09-16 11:15:30 -0300

    feat: change text and icon color when hovering the menu

--- src/components/TranslationMenu.tsx
+++ src/components/TranslationMenu.tsx
@@ -4,6 +4,7 @@ import { GlobeAltIcon } from "@heroicons/react/24/outline";
 
 export default function TranslationMenu() {
     const [isLangMenuOpen, setIsLangMenuOpen] = useState(false);
+    const [isHover, setIsHover] = useState(false);
 
     const t = useTranslations("Home.header");
     const translationText = t("translations");
@@ -14,22 +15,35 @@ export default function TranslationMenu() {
         <>
             <div
                 className="relative"
-                onMouseLeave={() => setIsLangMenuOpen(false)}>
+                onMouseOver={() => setIsHover(true)}
+                onMouseLeave={() => {
+                    setIsHover(false)
+                    setIsLangMenuOpen(false)
+                }}>
                 <button
                     className="mr-6 inline-flex items-center"
                     onClick={toggleLangMenu}>
-                    <GlobeAltIcon className="mr-1 size-6 text-wine/50 dark:text-bubblegum/50" />
-                    {translationText}
+                    <GlobeAltIcon className={`mr-1 size-6 
+                    ${isHover ? "text-wine dark:text-rose-200" : "text-wine/50 dark:text-bubblegum/50"}`} />
+                    <span className={`${isHover ? "dark:text-rose-200" : ""}`}>
+                        {translationText}
+                    </span>
                 </button>
                 <div
                     className={`
-                        absolute end-0 flex flex-col 
-                        overflow-hidden transition-all duration-300 bg-rose-300 
+                        absolute end-0 flex flex-col rounded-b-lg w-full 
+                        overflow-hidden transition-all duration-300 bg-rose-200 dark:bg-licorice 
                         ${isLangMenuOpen ? "max-h-24 p-4": "max-h-0 p-0"}
                     `}
                 >
-                    <a href="/pt">Português</a>
-                    <a href="/en">English</a>
+                    <a
+                        href="/pt"
+                        className="hover:text-rose-900 dark:hover:text-rose-200"
+                    >Português</a>
+                    <a
+                        href="/en"
+                        className="hover:text-rose-900 dark:hover:text-rose-200"
+                    >English</a>
                 </div>
             </div>
         </>
