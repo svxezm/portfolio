3474371247ac1d4d6235563089f14455d665d54f
svxezm <igorb.kuhl@gmail.com>
2025-10-09 11:11:14 -0300

    feat: move idiom section to another component

---
+++ src/components/Main/Idioms.tsx
@@ -0,0 +1,34 @@
+import { useTranslations } from "next-intl";
+
+export default function IdiomsDisplay() {
+  const t = useTranslations("Home.main.languages");
+  const title = t("titles.idioms");
+  const idioms = [
+    t("portuguese"),
+    t("english")
+  ];
+
+  return (
+    
+    <section
+        id="languages"
+        className={
+        `flex flex-col lg:justify-evenly items-baseline text-center 
+        mx-auto lg:w-full lg:px-8 mb-16 lg:mb-8`
+    }>
+      <div
+          className="flex flex-col flex-wrap mx-auto lg:mx-0"
+      >
+        <h3 className="w-full text-center lg:text-left mb-4">{title}</h3>
+        <div className="flex flex-col lg:text-left">
+          {idioms.map((title, titleIndex) => (
+              <p
+                  key={titleIndex}
+                  className="text-lg"
+              >{title}</p>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
